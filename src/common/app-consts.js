d42077edaf8188bb719d36517d319edb8db24292
sameh97 <sameh7462@gmail.com>
2025-07-21 22:29:47 +0300

    changed base url

--- src/common/app-consts.js
+++ src/common/app-consts.js
@@ -1,4 +1,5 @@
 export const AppConsts = {
   KEY_USER_TOKEN: "KEY_USER_TOKEN",
-  BASE_URL: "http://localhost:5000/api",
+  // BASE_URL: "http://localhost:5000/api",
+  BASE_URL: "http://ec2-44-198-165-219.compute-1.amazonaws.com:5000/api"
 };
